3ac164a6018084bcd65bce9d6dec3f71a92e9d0c
Marc Peral <m.peral@sapalomera.cat>
2022-10-17 18:55:12 +0200

    tweaked user initials and added user initials

--- vistes/index.vista.php
+++ vistes/index.vista.php
@@ -23,13 +23,16 @@
 		<div class="d-flex justify-content-between px-4 pt-4">
 			<h1 class=>Articles</h1>
 			<div class="btn-group dropstart">
-				<button type="button" class="btn btn-secondary rounded-circle " data-bs-toggle="dropdown" aria-expanded="false">
+				<button type="button" class="btn btn-secondary btn-sm rounded-circle m-0 px-4 " data-bs-toggle="dropdown" aria-expanded="false">
 					<?php
 					// require_once()
 					if (checkLogin()) {
-						echo "LG";
+						$nameInitial = $_SESSION['name-initial'];
+						$surnameInitial = $_SESSION['surname-initial'];
+
+						echo "<span class=\"my-0\">$nameInitial$surnameInitial</span>";
 					} else {
-						echo "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-person p-0 m-0 mx-2\" viewBox=\"0 0 16 16\">
+						echo "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-person p-0 m-0\" viewBox=\"0 0 16 16\">
 						<path d=\"M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z\" />
 					</svg>";
 					}
