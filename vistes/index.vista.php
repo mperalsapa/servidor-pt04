e06f62ef2ed5836233bdda5d00fe6018e4a9bea9
Marc Peral <m.peral@sapalomera.cat>
2022-10-17 18:56:18 +0200

    removed unused css

--- vistes/index.vista.php
+++ vistes/index.vista.php
@@ -6,7 +6,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="css/estils.css">
 	<?php include_once("internal/vistes/header.php"); ?>
 	<title>Paginació</title>
 </head>
