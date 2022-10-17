245bf60b1f9e930ed151a12e8f70626765d06b69
Marc Peral <m.peral@sapalomera.cat>
2022-10-17 18:53:33 +0200

    login view and login success view

--- vistes/login.vista.php
+++ vistes/login.vista.php
@@ -5,43 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar-se</title>
     <?php include_once("internal/vistes/header.php"); ?>
-    <title>Registre</title>
 </head>
 
 <body>
-    <div class="container py-5 h-100">
-        <div class="row justify-content-center align-items-center h-100">
-            <div class="col-12 col-lg-8 col-xl-6">
-                <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
-                    <div class="card-body p-4 p-md-5">
-                        <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Iniciar Sessió</h3>
-                        <form action="registre.php" method="POST">
-                            <div class="row">
-                                <div class=" mb-4 pb-2">
-                                    <div class="form-outline">
-                                        <input type="email" name="correu" class="form-control form-control-lg" value="<?php echo isset($email) ? $email : '' ?>" />
-                                        <label class="form-label" for="correu">Correu Electronic</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class=" mb-4 pb-2">
-                                    <div class="form-outline">
-                                        <input type="password" name="contrasenya" class="form-control form-control-lg" />
-                                        <label class="form-label" for="contrasenya">Contrassenya</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <p><?php echo isset($missatgeForm) ? $missatgeForm : '' ?></p>
-                            <div class="mt-4 pt-2">
-                                <input class="btn btn-primary btn-lg" type="submit" value="Iniciar Sessio" />
-                            </div>
-                        </form>
-                    </div>
+    <div class="bg-dark d-flex align-items-center justify-content-center" style="height:100vh; width:100vw;">
+        <div class="bg-white rounded col-8 col-md-5 col-lg-4  col-xxl-3">
+            <form class="h-50 align-middle m-4 <?php echo isset($invalidForm) ? 'was-validated' : '' ?>" action="login.php" method="POST">
+                <div class="form-group">
+                    <label for="">Correu Electronic</label>
+                    <input type="email" class="form-control" name="email" placeholder="Correu Electronic" required value="<?php echo isset($email) ? $email : '' ?>">
+                    <div class="invalid-feedback">Introdueix un correu electronic valid</div>
                 </div>
-            </div>
+                <div class="form-group mt-4">
+                    <label for="">Contrasenya</label>
+                    <input type="password" class="form-control" name="password" placeholder="Contrasenya" required>
+                    <div class="invalid-feedback">Introdueix una contrasenya</div>
+                </div>
+                <div class="mt-4">
+                    <div class="d-flex">
+                        <?php if (!empty($formResult)) {
+                            echo "<p class=\"bg-danger py-2 px-3 border rounded text-white\">";
+                            echo $formResult;
+                            echo "</p>";
+                        }
+                        ?>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
+
     </div>
     <?php include_once("internal/vistes/body_end.php"); ?>
 </body>
