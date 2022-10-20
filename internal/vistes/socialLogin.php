168a2a1d82622618c01db7a207adc5e66de73496
Marc <m.peral@sapalomera.cat>
2022-10-20 21:35:35 +0200

    added social login using google and github

---
+++ internal/vistes/socialLogin.php
@@ -0,0 +1,106 @@
+<?php
+
+include_once("hybridauth-3.8.2/src/autoload.php");
+
+
+
+function googleLogin(string $googleClientID, string $googleClientSecret): \Hybridauth\Provider\Google
+{
+    $config = [
+        'callback' => 'http://localhost/servidor/UF1/pt04/loginCallback.php?hauth.done=Google', // or Hybridauth\HttpClient\Util::getCurrentUrl()
+        'keys' => ['id' => $googleClientID, 'secret' => $googleClientSecret], // Your Github application credentials
+    ];
+    $googleAuth = new \Hybridauth\Provider\Google($config);
+    $googleAuth->authenticate();
+    return $googleAuth;
+}
+
+function getGoogleProfile(string $googleClientID, string $googleClientSecret): array
+{
+    $googleAuth = googleLogin($googleClientID, $googleClientSecret);
+    $googleAuth->authenticate();
+    try {
+        if ($googleAuth->authenticate("Google")) {
+            $userProfile = $googleAuth->getUserProfile();
+            $userInfo["email"] = $userProfile->email;
+            if (empty($userProfile->firstName)) {
+                $userInfo["name"] = $userProfile->displayName;
+            } else {
+                $userInfo["name"] = $userProfile->firstName;
+            }
+            $userInfo["surname"] = $userProfile->lastName;
+        } else {
+            die();
+        }
+    } catch (Hybridauth\Exception\HttpClientFailureException $e) {
+        echo 'Curl text error message : ' . $googleAuth->getHttpClient()->getResponseClientError();
+    } catch (\Exception $e) {
+        echo 'Oops! We ran into an unknown issue: ' . $e->getMessage();
+    }
+
+    $googleAuth->disconnect();
+    return $userInfo;
+}
+
+function githubLogin(string $githubClientID, string $githubClientSecret): \Hybridauth\Provider\GitHub
+{
+    $config = [
+        'callback' => 'http://localhost/servidor/UF1/pt04/loginCallback.php?hauth.done=GitHub', // or Hybridauth\HttpClient\Util::getCurrentUrl()
+        'keys' => ['id' => $githubClientID, 'secret' => $githubClientSecret], // Your Github application credentials
+        'curl_options' => [
+            CURLOPT_USERAGENT => 'mperalsapa'
+        ]
+    ];
+    $github = new Hybridauth\Provider\GitHub($config);
+    $github->authenticate();
+    return $github;
+}
+
+function getGithubProfile(string $githubClientID, string $githubClientSecret): array
+{
+    $github = githubLogin($githubClientID, $githubClientSecret);
+    try {
+        if ($github->authenticate("GitHub")) {
+            $userProfile = $github->getUserProfile();
+            $userInfo["email"] = $userProfile->email;
+            if (empty($userProfile->firstName)) {
+                $userInfo["name"] = $userProfile->displayName;
+            } else {
+                $userInfo["name"] = $userProfile->firstName;
+            }
+            $userInfo["surname"] = $userProfile->lastName;
+        } else {
+            die();
+        }
+    } catch (Hybridauth\Exception\HttpClientFailureException $e) {
+        echo 'Curl text error message : ' . $github->getHttpClient()->getResponseClientError();
+    } catch (\Exception $e) {
+        echo 'Oops! We ran into an unknown issue: ' . $e->getMessage();
+    }
+    $github->disconnect();
+    return $userInfo;
+}
+
+
+function socialLoginUser(array $userInfo)
+{
+    include_once("internal/db/mysql.php");
+    include_once("internal/db/session_manager.php");
+    include_once("internal/vistes/browser.php");
+    $pdo = getMysqlPDO();
+    if (userExists($pdo, $userInfo["email"])) {
+        echo "User exists, logging in...";
+        setUserLoggedinData($pdo, $userInfo["email"]);
+        redirectClient("index.php");
+        die();
+    }
+
+    echo "User does not exist, registering user.";
+    $register = addUser($pdo, $userInfo["name"], $userInfo["surname"], $userInfo["email"], "");
+    if ($register) {
+        setLoggedin(true);
+        redirectClient("index.php");
+        die();
+    }
+    echo "Error registering user";
+}
---
+++ loginCallback.php
@@ -0,0 +1,41 @@
+<?php
+
+
+echo "callback site\n";
+include_once("internal/vistes/socialLogin.php");
+include_once("internal/vistes/browser.php");
+include_once("env.php");
+
+
+if (!isset($_GET["hauth_done"])) {
+    die();
+}
+$authProvider = $_GET["hauth_done"];
+
+if (!isset($_GET["code"])) {
+    die();
+}
+$authCode = $_GET["code"];
+
+if (!isset($_GET["state"])) {
+    die();
+}
+$authState = $_GET["state"];
+
+switch ($authProvider) {
+    case 'Google':
+        $userInfo = getGoogleProfile($googleClientID, $googleClientSecret);
+        break;
+    case 'GitHub':
+        $userInfo = getGithubProfile($githubClientID, $githubClientSecret);
+        break;
+}
+
+
+if (!empty($userInfo)) {
+    socialLoginUser($userInfo);
+}
+
+print_r($userInfo, true);
+
+// redirectClient("login.php");
