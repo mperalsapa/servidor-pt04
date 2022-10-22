4b932cbc0569ca68f9eabf40ae9f099a058a1636
Marc <m.peral@sapalomera.cat>
2022-10-22 04:38:37 +0200

    fix webhook action when push new commit

--- webhook.php
+++ webhook.php
@@ -1,11 +1,11 @@
 <?php
 # webhook for project deployment
 if ($_GET["token"] == "WH9teDg98ppm2GjyxyYtgwbee563Cbgddv5tmuyX") {
-$cmd = shell_exec("find . -not \( -name 'webhook.php' -or -name 'env.php' \) -delete && git clone https://github.com/mperalsapa/servidor-pt04.git && rm -rf servidor-pt04/.git && mv servidor-pt04/* .");
-echo $cmd;
-echo 'Deployment sucess';
-} else {
-echo 'Error';
-}
+    // $cmd = shell_exec("find . -not \( -name 'webhook.php' -or -name 'env.php' \) -exec rm -r '{}' && git clone https://github.com/mperalsapa/servidor-pt04.git && rm -rf servidor-pt04/.git && mv servidor-pt04/* .");
+    $cmd = shell_exec("git pull");
 
-?>
+    echo $cmd;
+    echo 'Deployment sucess';
+} else {
+    echo 'Error';
+}
