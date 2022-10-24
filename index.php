c9dfafc126d48484316f755a240a8d560c57806d
Marc <m.peral@sapalomera.cat>
2022-10-24 08:56:34 +0000

    fix router redirect loop when baseurl is /

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include_once("env.php");
 
 $mux["/"] = "src/controllers/index.php";
 $mux["/index"] = "src/controllers/index.php";
@@ -16,18 +17,28 @@ $mux["/googleLogin"] = "src/controllers/loginCallback.php";
 $mux["/githubLogin"] = "src/controllers/loginCallback.php";
 $mux["/twitterLogin"] = "src/controllers/loginCallback.php";
 
+function route(string $url, array $mux, string $baseUrl) {
+        if (isset($mux[$url])) {
+                $controller = $mux[$url];
+                include_once($controller);
+                die();
+        } else {
+                include_once("src/internal/viewFunctions/browser.php");
+                redirectClient($baseUrl);
+        }
+
+}
 
 $parsedUri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-include_once("env.php");
+
+if (!isset($baseUrl)) {
+        route($parsedUri, $mux, $baseUrl);
+}
+if (empty($baseUrl) || $baseUrl == "/") {
+        route($parsedUri, $mux, $baseUrl);
+}
 
 $pattern = "/" . str_replace("/", "\/", $baseUrl) . "(.*)$/";
 preg_match($pattern, $parsedUri, $match);
 $parsedUri = $match[1];
-
-if (isset($mux[$parsedUri])) {
-        $controller = $mux[$parsedUri];
-        include_once($controller);
-} else {
-        include_once("src/internal/viewFunctions/browser.php");
-        redirectClient($baseUrl);
-}
+route($parsedUri,$mux, $baseUrl);
