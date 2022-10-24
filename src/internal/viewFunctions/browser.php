986fdbbe934bdcb45b3fb4224699013b2bbc08fe
Marc <m.peral@sapalomera.cat>
2022-10-24 08:55:13 +0000

    fix repeated backslash

--- src/internal/viewFunctions/browser.php
+++ src/internal/viewFunctions/browser.php
@@ -2,9 +2,13 @@
 // Marc Peral 2 DAW
 
 // funcio basica per redirigir el client a una URL especificada sobre la url base del lloc
-function redirectClient(string $url): void
+function redirectClient(string $url)
 {
     include("env.php");
-    header("Location: " . $baseUrl . $url);
+
+    $redirect = $baseUrl . $url;
+    $redirect = str_replace("//","/", $redirect);
+
+    header("Location: " . $redirect);
     die();
 }
