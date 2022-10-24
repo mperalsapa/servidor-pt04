f931c4081c55c8f9a92a24f16bc5e2af2cbede86
Marc <m.peral@sapalomera.cat>
2022-10-24 10:18:16 +0200

    fix added base url to client redirections

--- src/internal/viewFunctions/browser.php
+++ src/internal/viewFunctions/browser.php
@@ -1,9 +1,10 @@
 <?php
 // Marc Peral 2 DAW
 
-// funcio basica per redirigir el client a una URL especificada
+// funcio basica per redirigir el client a una URL especificada sobre la url base del lloc
 function redirectClient(string $url): void
 {
-    header("Location: $url");
+    include("env.php");
+    header("Location: " . $baseUrl . $url);
     die();
 }
