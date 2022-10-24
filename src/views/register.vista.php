070e4e331453c536c2c6f11c4fa0e0317583df78
Marc Peral <m.peral@sapalomera.cat>
2022-10-24 20:28:25 +0200

    twak added bg color in css before bootstrap

--- src/views/register.vista.php
+++ src/views/register.vista.php
@@ -9,7 +9,7 @@
     <title>Registre</title>
 </head>
 
-<body class="m-0 p-0 bg-dark">
+<body class="m-0 p-0 bg-dark" style="background-color:#212529">
     <div class="d-flex flex-column align-items-center justify-content-center">
         <a class="my-4 d-flex align-items-center justify-content-center btn btn-secondary col-12 col-sm-8 col-md-6 col-lg-5 col-xxl-3" href="index">
             <span class="text-white m-0 fs-5"><i class="bi bi-house"></i> ARTICLES DE PEL·LÍCULES</span>
