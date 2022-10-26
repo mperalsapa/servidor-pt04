d1a0db092906930af61e1e05eac48ac9c4741bb7
Marc <m.peral@sapalomera.cat>
2022-10-26 19:19:35 +0200

    added returnalert instead of error

--- src/internal/viewFunctions/formError.php
+++ src/internal/viewFunctions/formError.php
@@ -7,3 +7,28 @@ function retornarError(string $error, string $vista)
     include_once($vista);
     die();
 }
+
+function returnAlert(string $message, string $type, string $vista, ?array $viewData = array())
+{
+    $alertMessage = $message;
+    $alertType = $type;
+    $alertIcon = "";
+
+    switch ($type) {
+        case 'primary':
+        case 'info':
+            $alertIcon = "<i class=\"bi bi-info-circle\"></i>";
+            break;
+        case 'success':
+            $alertIcon = "<i class=\"bi bi-check-circle\"></i>";
+            break;
+        case 'warning':
+            $alertIcon = "<i class=\"bi bi-exclamation-triangle\"></i>";
+            break;
+        case 'danger':
+            $alertIcon = "<i class=\"bi bi-exclamation-triangle\"></i>";
+            break;
+    }
+    include_once($vista);
+    die();
+}
