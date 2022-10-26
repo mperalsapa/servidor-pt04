32edeebd8008c3f3fbf334c3a38ba642e780a262
Marc <m.peral@sapalomera.cat>
2022-10-26 19:54:36 +0200

    updated webhook to get token from env file

--- webhook.php
+++ webhook.php
@@ -1,6 +1,7 @@
 <?php
 # webhook for project deployment
-if ($_GET["token"] == "WH9teDg98ppm2GjyxyYtgwbee563Cbgddv5tmuyX") {
+include("env.php");
+if ($_GET["token"] == $webhookToken) {
     // $cmd = shell_exec("find . -not \( -name 'webhook.php' -or -name 'env.php' \) -exec rm -r '{}' && git clone https://github.com/mperalsapa/servidor-pt04.git && rm -rf servidor-pt04/.git && mv servidor-pt04/* .");
     $cmd = shell_exec("git pull");
 
