807eeefdf8b3ed9340187273c37fe9311e0c3194
Marc Peral <m.peral@sapalomera.cat>
2022-11-02 16:48:32 +0100

    added confirmation when deleting article

--- src/internal/viewFunctions/articles.php
+++ src/internal/viewFunctions/articles.php
@@ -15,7 +15,27 @@ function printArticlesbyUserId($articles)
                 if ($userId == $row["autor"]) {
                     echo "<div class=\"mt-3\">";
                     echo "<a class=\"btn btn-sm btn-outline-primary me-3\" href=\"write-article?id=" . $row["id"] . "\"><i class=\"bi bi-pencil\"></i> Editar</a>";
-                    echo "<a class=\"btn btn-sm btn-outline-danger \" href=\"delete-article?id=" . $row["id"] . "\"><i class=\"bi bi-trash\"></i> Esborrar</a>";
+                    echo "
+                        <div class=\"modal fade\" id=\"articleDelete\" tabindex=\"-1\" aria-labelledby=\"articleDeleteLabel\" aria-hidden=\"true\">
+                            <div class=\"modal-dialog\">
+                                <div class=\"modal-content\">
+                                    <div class=\"modal-header\">
+                                        <h5 class=\"modal-title\" id=\"articleDeleteLabel\">Esborrar Article</h5>
+                                        <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>
+                                    </div>
+                                    <div class=\"modal-body\">
+                                        Estas segur que vols esborrar aquest article? No es podra recuperar una vegada esborrat!
+                                    </div>
+                                    <div class=\"modal-footer\">
+                                        <button type=\"button\" class=\"btn btn-primary\" data-bs-dismiss=\"modal\">Cancel·lar</button>
+                                        <a class=\"btn btn-danger \" href=\"delete-article?id=" . $row["id"] . "\"><i class=\"bi bi-trash\"></i> Esborrar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>";
+                    echo "<button type=\"button\" class=\"btn btn-sm btn-outline-danger\" data-bs-toggle=\"modal\" data-bs-target=\"#articleDelete\">
+                            <i class=\"bi bi-trash\"></i> Esborrar
+                        </button>";
                     echo "</div>";
                 }
             }
